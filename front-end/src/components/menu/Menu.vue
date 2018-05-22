cdc77a624ff32e91504855420ef823d1ef028fa4
r0630049 <dries.vanbilloen@gmail.com>
2018-05-22 23:12:56 +0200

    Installed the vue cli and the vuetify framework + added menu component

---
+++ front-end/src/components/menu/Menu.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-navigation-drawer permanent>
+    <v-toolbar flat>
+      <v-list>
+        <v-list-tile></v-list-tile>
+      </v-list>
+    </v-toolbar>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'Menu'
+}
+</script>
+
+<style lang="css">
+</style>
